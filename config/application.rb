63aed8904968e18c25c6cc4f4f75fd29bb8d6865
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-02-23 17:52:38 +0900

    add: アプリケーションの国際化

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,10 @@ module CdpWebManyoTask
         request_specs: false
     end
 
+    config.i18n.default_locale = :ja
+    #　パスを通す
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
