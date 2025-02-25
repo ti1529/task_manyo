542da3437a14286ca312babb85a632cf889d6206
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-02-25 16:14:28 +0900

    機能要件

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ApplicationRecord
   validates :content, presence: true
   validates :deadline_on, presence: true
   validates :priority, presence: true
+  validates :status, presence: true
 
   enum priority: { low: 0, middle: 1, high: 2 }
   enum status: { not_started: 0, in_progress: 1, completed: 2}
