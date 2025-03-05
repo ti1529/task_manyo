07f9673636c6d638e33f2663b0940b082f548136
IIDA Tatsuya <t.i.gjad.152978@gmail.com>
2025-03-05 16:09:36 +0900

    fix: 初期データの定義

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,42 @@
 # userの作成
-user = FactoryBot.create(:user)
-admin = FactoryBot.create(:admin)
 
+user = User.create!(
+  name: "user_1",
+  email: "user_1@mail.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+admin = User.create!(
+  name: "admin_1",
+  email: "admin_1@mail.com",
+  password: "password",
+  password_confirmation: "password",
+  admin: true
+)
 
 # 一般ユーザのtaskを作成
 50.times do |i|
-  Task.create!(title: "title_#{i +1}",
-                content: "content_#{i +1}",
-                created_at: (i +1).hour.ago,
-                deadline_on: (i +1).days.from_now,
-                priority: (i +1) % 3,
-                status: (i +1) % 3,
-                user_id: user.id )
+  Task.create!(
+    title: "title_#{i +1}",
+    content: "content_#{i +1}",
+    created_at: (i +1).hour.ago,
+    deadline_on: (i +1).days.from_now,
+    priority: (i +1) % 3,
+    status: (i +1) % 3,
+    user_id: user.id 
+  )
 end
 
 # 管理者のタスクを作成
 50.times do |i|
-  Task.create!(title: "title_#{i +1}",
-                content: "content_#{i +1}",
-                created_at: (i +1).hour.ago,
-                deadline_on: (i +1).days.from_now,
-                priority: (i +1) % 3,
-                status: (i +1) % 3,
-                user_id: admin.id )
+  Task.create!(
+    title: "title_#{i +1}",
+    content: "content_#{i +1}",
+    created_at: (i +1).hour.ago,
+    deadline_on: (i +1).days.from_now,
+    priority: (i +1) % 3,
+    status: (i +1) % 3,
+    user_id: admin.id
+  )
 end
